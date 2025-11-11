3a18e8cc5510ac31fea27c84cf2d7deb8febe467
VUTIENTHANG2k4 <thangvuit18@gmail.com>
2025-11-11 22:54:37 +0700

    Update login and dashboard

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -7,12 +7,7 @@ module.exports = function (api) {
         "module:react-native-dotenv",
         {
           "moduleName": "@env",
-          "path": ".env",
-          "blocklist": null,
-          "allowlist": null,
-          "safe": false,
-          "allowUndefined": true,
-          "verbose": false
+          "path": ".env"
         }
       ]
     ],
