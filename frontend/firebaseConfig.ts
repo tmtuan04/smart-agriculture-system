9e6c8e91fef5acdad6f428e3fd11550f893d79ab
VUTIENTHANG2k4 <thangvuit18@gmail.com>
2025-11-11 10:10:53 +0700

    secondcommit

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -1,17 +1,27 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+// eslint-disable-next-line import/no-unresolved
+} from "@env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBAkYTM4BvLNgIWwlRB2t9NBakU2jkqPto",
-  authDomain: "smart-agriculture-system-9b29a.firebaseapp.com",
-  projectId: "smart-agriculture-system-9b29a",
-  storageBucket: "smart-agriculture-system-9b29a.appspot.com",
-  messagingSenderId: "307178711489",
-  appId: "1:307178711489:web:55de29d4edd63c69bda740",
-  measurementId: "G-KWT213W5RB"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
-// ✅ Khởi tạo chỉ 1 lần (rất quan trọng trong Expo)
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
